f5ffcc6d54fb86aac1e96115363ab50f988ab084
bjon1 <bcjon3@gmail.com>
2023-09-30 20:27:43 -0400

    Rearrange ultrasound functions into a class for code reusability

--- Test_Ultrasonic.py
+++ Test_Ultrasonic.py
@@ -10,49 +10,62 @@ def check_distance(distance):
         return False
     return True
 
+class USensor:
+    def __init__(self, trig, echo):
+        self.trig = trig
+        self.echo = echo
 
-# assign pins
-GPIO_TRIGGER = 16
-GPIO_ECHO = 18
+    def configure(self):
+        GPIO.setup(self.trig, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
 
-# configure board and pins
-GPIO.setmode(GPIO.BOARD) # GPIO Mode (BOARD / BCM)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+        # start sensor
+        GPIO.output(self.trig, GPIO.LOW)
+        time.sleep(1)
 
-# start sensor
-GPIO.output(GPIO_TRIGGER, GPIO.LOW)
-time.sleep(1)
+    def start_ultrasound(self):
+        self.configure()
 
-while True:
-    try:
-        # send pulse
-        GPIO.output(GPIO_TRIGGER, GPIO.HIGH)
-        time.sleep(0.00001)
-        GPIO.output(GPIO_TRIGGER, GPIO.LOW)
+        while True:
+            try:
+                # send pulse
+                GPIO.output(self.trig, GPIO.HIGH)
+                time.sleep(0.00001)
+                GPIO.output(self.trig, GPIO.LOW)
 
-        # receive response and calculate distance
-        if GPIO.wait_for_edge(GPIO_ECHO, GPIO.RISING, timeout=100):
-            t_start = time.time()
-            if GPIO.wait_for_edge(GPIO_ECHO, GPIO.FALLING, timeout=100):
-                t_end = time.time()
-                t = t_end - t_start
-                distance = ((t * 34300) / 2)/100
+                # receive response and calculate distance
+                if GPIO.wait_for_edge(self.echo, GPIO.RISING, timeout=100):
+                    t_start = time.time()
+                    if GPIO.wait_for_edge(self.echo, GPIO.FALLING, timeout=100):
+                        t_end = time.time()
+                        t = t_end - t_start
+                        distance = ((t * 34300) / 2)/100
 
-                print(f"Distance: {distance} meters")
+                        print(f"Sensor1 Distance: {distance} meters")
 
-                if not check_distance(distance):
-                    stop_movement()
-            else:
-                stop_movement()
-                print('Falling edge timeout')
-        else:
-            print('Rising edge timeout')
-    except:
-        #Stop All Movement
-        GPIO.cleanup()
+                        if not check_distance(distance):
+                            stop_movement()
+                    else:
+                        stop_movement()
+                        print('Sensor1 Falling edge timeout')
+                else:
+                    print('Sensor1 Rising edge timeout')
+            except:
+                #Stop All Movement
+                GPIO.cleanup()
 
-    time.sleep(0.5)
+            time.sleep(0.5)
+
+
+usensor1 = USensor(trig=16, echo=18)
+usensor2 = USensor(trig=19, echo=21)
+GPIO.setmode(GPIO.BOARD) #GPIO Mode BOARD
+usensor1.start_ultrasound()
+usensor2.start_ultraasound()
+
+
+
+
 
 
     
