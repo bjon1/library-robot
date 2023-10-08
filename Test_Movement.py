9ef700186b2078340607fa2b3015fa979cdb18b1
Benjamin Jon <bcjon3@gmail.com>
2023-10-08 13:30:40 -0400

    Add GUI and Set Frequency & Duty Cycle

--- Test_Movement.py
+++ Test_Movement.py
@@ -1,116 +1,98 @@
 import RPi.GPIO as GPIO
+import tkinter as tk
 import time
 import keyboard
 
 output_pin1 = 32 
 output_pin2 = 33
-digOut_pin1 = 15
-digOut_pin2 = 16
+digout_pin1 = 15
+digout_pin2 = 16
 
-print("OUTPUT1", output_pin1)
-print("OUTPUT2", output_pin2)
+# Globally scoped variables
+p1 = None
+p2 = None
 
-# Pin Setup:
-# Board pin-numbering scheme
-GPIO.setmode(GPIO.BOARD)
-# set PWM as an output pin with optional initial state of HIGH
-GPIO.setup(output_pin1, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(output_pin2, GPIO.OUT, initial=GPIO.HIGH)
-# set digitalOutput as an output pin with optional initial state of LOW
-GPIO.setup(digOut_pin1, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(digOut_pin2, GPIO.OUT, initial=GPIO.LOW)
-# create pwm objects
-p1 = GPIO.PWM(output_pin1, 50)
-p2 = GPIO.PWM(output_pin2, 50)
+def main():
+    configure_pwm()
+    create_gui()
 
+def configure_pwm()
+    # Pin Setup:
+    # Board pin-numbering scheme
+    GPIO.setmode(GPIO.BOARD)
+    # set PWM as an output pin with optional initial state of HIGH
+    GPIO.setup(output_pin1, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(output_pin2, GPIO.OUT, initial=GPIO.HIGH)
+    # set digitalOutput as an output pin with optional initial state of LOW
+    GPIO.setup(digout_pin1, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(digout_pin2, GPIO.OUT, initial=GPIO.LOW)
+    # create pwm objects
+    p1 = GPIO.PWM(output_pin1, 25000)
+    p2 = GPIO.PWM(output_pin2, 25000)
 
-''' WARNING: Keyboard controls will not work. 
-It is just a prototype of how it should look like. I will add an event listener, simple graphical user interface with buttons, or RTOS later using the tkinter module
-'''
+def create_gui():
+    # Create the main window
+    root = tk.Tk()
+    root.title("Henry Control")
 
-# Define the key you want to monitor  
-forward = "w"
-left = "a"
-reverse = "s"
-right = "d"
-stop = "q"
+    # Create a frame to contain the buttons
+    button_frame = tk.Frame(root)
+    button_frame.pack(padx=20, pady=20)
 
-directions = {
-    forward: "forward",
-    left: "left",
-    reverse: "reverse",
-    right: "right",
-    quit: "quit"
-}
+    # Create and configure buttons
+    button_forward = tk.Button(button_frame, text="Forward", command=move("forward")
+    button_right = tk.Button(button_frame, text="Right", command=move("right")
+    button_left = tk.Button(button_frame, text="Left", command=move("left"))
+    button_reverse = tk.Button(button_frame, text="Reverse", command=move("reverse")
+    button_stop = tk.Button(button_frame, text="Stop", command=move("stop"))
 
-while True:
-    for key in directions.keys():
-        if keyboard.is_pressed(key):
-            direction = directions.getValue(key)
-            move(direction)
-            print("Moved", direction)
-        elif keyboard.is_pressed(stop):
-            print("Stopped")
-            stop()
+    # Pack the buttons
+    button_forward.pack(side=tk.LEFT, padx=10)
+    button_right.pack(side=tk.LEFT, padx=10)
+    button_left.pack(side=tk.LEFT, padx=10)
+    button_reverse.pack(side=tk.LEFT, padx=10)
+    button_stop.pack(side=tk.LEFT, padx=10)
 
+    # Start the Tkinter main loop
+    root.mainloop()
 
 def move(direction):
     # determine the direction and move accordingly
     stop() # This may not be needed or may cause bugs
     match direction:
         case "forward":
-            activate_pwm() 
             # Need to check if activating PWM while it is already running will cause issues. If so, we need to check if PWM is active before calling this function
-            GPIO.output(digOut_pin1, GPIO.LOW) # These values need to be checked
-            GPIO.output(digOut_pin2, GPIO.LOW)
+            GPIO.output(digout_pin1, GPIO.LOW) # These values need to be checked
+            GPIO.output(digout_pin2, GPIO.LOW)
         case "left":
-            activate_pwm()
-            GPIO.output(digOut_pin1, GPIO.HIGH)
-            GPIO.output(digOut_pin2, GPIO.LOW)
+            GPIO.output(digout_pin1, GPIO.HIGH)
+            GPIO.output(digout_pin2, GPIO.LOW)
         case "reverse":
-            activate_pwm()
-            GPIO.output(digOut_pin1, GPIO.HIGH)
-            GPIO.output(digOut_pin2, GPIO.HIGH)
+            GPIO.output(digout_pin1, GPIO.HIGH)
+            GPIO.output(digout_pin2, GPIO.HIGH)
         case "right":
-            activate_pwm()
-            GPIO.output(digOut_pin1, GPIO.LOW)
-            GPIO.output(digOut_pin2, GPIO.HIGH)
-        case _ :
+            GPIO.output(digout_pin1, GPIO.LOW)
+            GPIO.output(digout_pin2, GPIO.HIGH)
+        case "stop" :
             stop()
+        case _ :
             raise ValueError("This movement does not exist")
+    activate_pwm() # Orient the direction first, and then activate pwm signal
 
 def stop():
     p1.stop()
     p2.stop()
     
 def activate_pwm():
-    print("PWM running. Press CTRL+C to exit.")
-    # set pwm values
-    val = 25
-    incr = 5
     # start pwm
-    p1.start(val)
-    p2.start(val)
-    try:
-        while True:
-            time.sleep(0.05)
-            if val >= 100:
-                print(">=100")
-                incr = -incr
-            if val <= 0:
-                print("<=0")
-                incr = -incr
-            val += incr
-            p1.ChangeDutyCycle(val)
-            p2.ChangeDutyCycle(val)
-    finally:
-        p1.stop()
-        p2.stop()
-        GPIO.cleanup(output_pin1)
-        GPIO.cleanup(output_pin2)
-        GPIO.cleanup(digOut_pin1)
-        GPIO.cleanup(digOut_pin2)
+    p1.start(0)
+    p2.start(0)
+    print("PWM running. Press CTRL+C to exit.")
+    p1.ChangeDutyCycle(75)
+    p2.ChangeDutyCycle(75)
 
+if __name__ == "__main__":
+    main()
         
         
                 
