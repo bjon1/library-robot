bf314438170b0ff00f36e3e356984021285ab368
Benjamin Jon <bcjon3@gmail.com>
2023-10-23 01:46:57 -0400

    Create madgwick_algorithm.py

---
+++ MPU/madgwick_algorithm.py
@@ -0,0 +1,163 @@
+'''
+    Credit to Jonas Böer, jonas.boeer@student.kit.edu, for making the MadgwickAHRS and Quaternion Classes
+'''
+import math
+import warnings
+import numpy as np
+from numpy.linalg import norm
+
+class Quaternion:
+    """
+    A simple class implementing basic quaternion arithmetic.
+    """
+    def __init__(self, w_or_q, x=None, y=None, z=None):
+        """
+        Initializes a Quaternion object
+        :param w_or_q: A scalar representing the real part of the quaternion, another Quaternion object or a
+                    four-element array containing the quaternion values
+        :param x: The first imaginary part if w_or_q is a scalar
+        :param y: The second imaginary part if w_or_q is a scalar
+        :param z: The third imaginary part if w_or_q is a scalar
+        """
+        self._q = np.array([1, 0, 0, 0])
+
+        if x is not None and y is not None and z is not None:
+            w = w_or_q
+            q = np.array([w, x, y, z])
+        elif isinstance(w_or_q, Quaternion):
+            q = np.array(w_or_q.q)
+        else:
+            q = np.array(w_or_q)
+            if len(q) != 4:
+                raise ValueError("Expecting a 4-element array or w x y z as parameters")
+
+        self.q = q
+
+    def to_euler_angles_ZYX(self):
+        pitch = np.arcsin(2 * self[1] * self[2] + 2 * self[0] * self[3])
+        if np.abs(self[1] * self[2] + self[3] * self[0] - 0.5) < 1e-8:
+            roll = 0
+            yaw = 2 * np.arctan2(self[1], self[0])
+        elif np.abs(self[1] * self[2] + self[3] * self[0] + 0.5) < 1e-8:
+            roll = -2 * np.arctan2(self[1], self[0])
+            yaw = 0
+        else:
+            roll = np.arctan2(2 * self[0] * self[1] - 2 * self[2] * self[3], 1 - 2 * self[1] ** 2 - 2 * self[3] ** 2)
+            yaw = np.arctan2(2 * self[0] * self[2] - 2 * self[1] * self[3], 1 - 2 * self[2] ** 2 - 2 * self[3] ** 2)
+        return roll, pitch, yaw
+
+    def to_euler_angles_XYZ(self):
+        roll = np.arctan2(-2 * (self[2] * self[3] - self[0] * self[1]), self[0] ** 2 - self[1] ** 2 - self[2] ** 2 + self[3] ** 2)
+        pitch = np.arcsin(2 * (self[1] * self[3] + self[0] * self[1]))
+        yaw = np.arctan2(-2 * (self[1] * self[2] - self[0] * self[3]), self[0] ** 2 + self[1] ** 2 - self[2] ** 2 - self[3] ** 2)
+        return roll, pitch, yaw
+
+
+class MadgwickAHRS:
+    samplePeriod = 1/256
+    quaternion = Quaternion(1, 0, 0, 0)
+    beta = 1
+    zeta = 0
+
+    def __init__(self, sampleperiod=None, quaternion=None, beta=None, zeta=None):
+        """
+        Initialize the class with the given parameters.
+        :param sampleperiod: The sample period
+        :param quaternion: Initial quaternion
+        :param beta: Algorithm gain beta
+        :param beta: Algorithm gain zeta
+        :return:
+        """
+        if sampleperiod is not None:
+            self.samplePeriod = sampleperiod
+        if quaternion is not None:
+            self.quaternion = quaternion
+        if beta is not None:
+            self.beta = beta
+        if zeta is not None:
+            self.zeta = zeta
+
+
+    def update(self, gyroscope, accelerometer, magnetometer):
+        """
+        Perform one update step with data from a AHRS sensor array
+        :param gyroscope: A three-element array containing the gyroscope data in radians per second.
+        :param accelerometer: A three-element array containing the accelerometer data. Can be any unit since a normalized value is used.
+        :param magnetometer: A three-element array containing the magnetometer data. Can be any unit since a normalized value is used.
+        :return:
+        """
+        q = self.quaternion
+
+        gyroscope = np.array(gyroscope, dtype=float).flatten()
+        accelerometer = np.array(accelerometer, dtype=float).flatten()
+        magnetometer = np.array(magnetometer, dtype=float).flatten()
+
+        # Normalise accelerometer measurement
+        if norm(accelerometer) is 0:
+            warnings.warn("accelerometer is zero")
+            return
+        accelerometer /= norm(accelerometer)
+
+        # Normalise magnetometer measurement
+        if norm(magnetometer) is 0:
+            warnings.warn("magnetometer is zero")
+            return
+        magnetometer /= norm(magnetometer)
+
+        h = q * (Quaternion(0, magnetometer[0], magnetometer[1], magnetometer[2]) * q.conj())
+        b = np.array([0, norm(h[1:3]), 0, h[3]])
+
+        # Gradient descent algorithm corrective step
+        f = np.array([
+            2*(q[1]*q[3] - q[0]*q[2]) - accelerometer[0],
+            2*(q[0]*q[1] + q[2]*q[3]) - accelerometer[1],
+            2*(0.5 - q[1]**2 - q[2]**2) - accelerometer[2],
+            2*b[1]*(0.5 - q[2]**2 - q[3]**2) + 2*b[3]*(q[1]*q[3] - q[0]*q[2]) - magnetometer[0],
+            2*b[1]*(q[1]*q[2] - q[0]*q[3]) + 2*b[3]*(q[0]*q[1] + q[2]*q[3]) - magnetometer[1],
+            2*b[1]*(q[0]*q[2] + q[1]*q[3]) + 2*b[3]*(0.5 - q[1]**2 - q[2]**2) - magnetometer[2]
+        ])
+        j = np.array([
+            [-2*q[2],                  2*q[3],                  -2*q[0],                  2*q[1]],
+            [2*q[1],                   2*q[0],                  2*q[3],                   2*q[2]],
+            [0,                        -4*q[1],                 -4*q[2],                  0],
+            [-2*b[3]*q[2],             2*b[3]*q[3],             -4*b[1]*q[2]-2*b[3]*q[0], -4*b[1]*q[3]+2*b[3]*q[1]],
+            [-2*b[1]*q[3]+2*b[3]*q[1], 2*b[1]*q[2]+2*b[3]*q[0], 2*b[1]*q[1]+2*b[3]*q[3],  -2*b[1]*q[0]+2*b[3]*q[2]],
+            [2*b[1]*q[2],              2*b[1]*q[3]-4*b[3]*q[1], 2*b[1]*q[0]-4*b[3]*q[2],  2*b[1]*q[1]]
+        ])
+        step = j.T.dot(f)
+        step /= norm(step)  # normalise step magnitude
+
+        # Gyroscope compensation drift
+        gyroscopeQuat = Quaternion(0, gyroscope[0], gyroscope[1], gyroscope[2])
+        stepQuat = Quaternion(step.T[0], step.T[1], step.T[2], step.T[3])
+
+        gyroscopeQuat = gyroscopeQuat + (q.conj() * stepQuat) * 2 * self.samplePeriod * self.zeta * -1
+
+        # Compute rate of change of quaternion
+        qdot = (q * gyroscopeQuat) * 0.5 - self.beta * step.T
+
+        # Integrate to yield quaternion
+        q += qdot * self.samplePeriod
+        self.quaternion = Quaternion(q / norm(q))  # normalise quaternion
+
+
+def calculate_orientation(accel_data, gyro_data, mag_data):
+    # Create an instance of the MadgwickAHRS class, with a sample period of 256 times per second
+    madgwick_filter = MadgwickAHRS(sampleperiod=1/256)
+
+    # Update the Madgwick filter with new sensor data
+    madgwick_filter.update(gyro_data, accel_data, mag_data)
+
+    # Get the current quaternion output from the filter
+    # A quaternion is a complex number that can be used to represent orientation in 3D space
+    q = madgwick_filter.quaternion
+
+    # Convert quaternion to Euler angles
+    roll, pitch, yaw = q.to_euler_angles_XYZ()
+
+    # Convert to degrees
+    yaw *= 180.0 / math.pi
+    pitch *= 180.0 / math.pi
+    roll *= 180.0 / math.pi
+
+    return yaw, pitch, roll
