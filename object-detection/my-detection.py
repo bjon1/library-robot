7853fc306e89c174a9503c508e9fa8ee7402e58a
bjon1 <bcjon3@gmail.com>
2024-02-23 17:45:13 -0500

    Changes in my-detection

--- object-detection/my-detection.py
+++ object-detection/my-detection.py
@@ -1,13 +1,17 @@
-import jetson_inference
-import jetson_utils
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput
 
-net = jetson_inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = jetson_utils.gstCamera(1280, 720, '/dev/video0')
-display = jetson_utils.glDisplay()
+net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = videoSource("/dev/video0")
+display = videoOutput("display://0")
 
-while display.IsOpen():
-    img, width, height = camera.CaptureRGBA()
-    detections = net.Detect(img, width, height)
-    display.RenderOnce(img, width, height)
-    display.SetTitle("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
-    
+while display.IsStreaming():
+	img = camera.Capture()
+	
+	if img is None:
+		continue
+
+	detections = net.Detect(img)
+
+	display.Render(img)
+	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
