2226625d80538d68d856c7521f9287d2a2a324c8
nebjamin <jonb1@newpaltz.edu>
2024-03-01 21:45:25 -0500

    Add some AI testing files

---
+++ object-detection/yolotest.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO('yolov8n.pt')
+
+# Run Inference
+results = model(source=0, show=True, conf=0.4, save=True)
