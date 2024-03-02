1366fffab8c05d8bfc9d4bb86e9236c6987456fa
nebjamin <jonb1@newpaltz.edu>
2024-03-01 22:20:22 -0500

    Set up server side

---
+++ surface_pro_4/surface_pro_web_interface/server/index.js
@@ -0,0 +1,46 @@
+/* 
+    The package.json file is located in the root directory of the project. 
+    Note that type: module is added to the package.json file to enable ES6 module syntax.
+*/
+
+import dotenv from 'dotenv'
+dotenv.config()
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(express.static(path.join(dirname(fileURLToPath(import.meta.url)), '../client/dist'))) // serve static files from the client side
+app.use(cors({
+    origin: 'http://localhost:3000', // origin of the request
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    credentials: true
+}))
+
+
+
+// Add Controllers
+
+
+
+app.get('*', (req, res) => { //catch all case for serving the static files, in case the user refreshes the page
+    res.sendFile(path.join(dirname(fileURLToPath(import.meta.url)), '../client/dist/index.html'))
+})
+
+//Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err)
+    const error_msg = {
+        status: err.status || 500,
+        message: err.message || 'Internal Server Error',
+        isSuccess: false
+    }
+    res.status(error_msg.status).json(error_msg)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/`)
+})
